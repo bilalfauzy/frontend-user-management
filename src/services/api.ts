78b1bc173796e1e8f888b378daa051bc309c5ef1
bilalfauzi <bilalfauzi@gmail.com>
2025-02-20 16:15:56 +0700

    all done

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,6 @@
 import axios from "axios";
 
-const API_URL =
-  import.meta.env.VITE_API_URL || "https://jsonplaceholder.typicode.com/users";
+const API_URL = "https://jsonplaceholder.typicode.com/users";
 
 export const fetchUsers = async () => {
   const response = await axios.get(API_URL);
